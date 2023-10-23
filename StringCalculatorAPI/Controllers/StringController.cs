3ffe18217a0d230542b508ac1cd34b23c551c29a
EmilioDeniz <welidrams112@gmail.com>
2023-10-23 09:01:18 +0100

    añadida la API a la solución

---
+++ StringCalculatorAPI/Controllers/StringController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StringCalculator;
+
+namespace StringCalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StringController : ControllerBase
+    {
+
+        StringCalculatorClass calculator = new StringCalculatorClass();
+
+        [HttpGet("{sum}")]
+        public string Get(string sum)
+        {
+            return calculator.add(sum).ToString(); ;
+        }
+
+    }
+}
