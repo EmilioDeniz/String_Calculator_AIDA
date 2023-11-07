bb45ede278dfa1edc355daf02768cbe8055da4e8
EmilioDeniz <welidrams112@gmail.com>
2023-11-07 12:42:52 +0000

    HealthCheck añadido

---
+++ StringCalculatorAPI/HistoryHealthCheck.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Text;
+using System.Text.Json;
+
+namespace StringCalculatorAPI
+{
+    public class HistoryHealthCheck : IHealthCheck
+    {
+        private string path;
+        public HistoryHealthCheck(string path) {
+            this.path = path;
+        }
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var isHealthy = Directory.Exists(Path.GetDirectoryName(path));
+
+            if (isHealthy)
+            {
+                var data = new Dictionary<string, object>{ { "path", path } };
+                return Task.FromResult(HealthCheckResult.Healthy("Healthy", data));
+            }
+
+            return Task.FromResult(HealthCheckResult.Unhealthy("UnHealthy"));
+        }
+
+        public static Task writeJson(HttpContext context, HealthReport healthReport)
+        {
+            context.Response.ContentType = "application/json; charset=utf-8";
+
+            var options = new JsonWriterOptions { Indented = true };
+
+            using var memoryStream = new MemoryStream();
+            using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
+            {
+                jsonWriter.WriteStartObject();
+                jsonWriter.WriteString("status", healthReport.Status.ToString());
+                jsonWriter.WriteStartObject("results");
+
+                foreach (var healthReportEntry in healthReport.Entries)
+                {
+                    jsonWriter.WriteStartObject(healthReportEntry.Key);
+                    jsonWriter.WriteString("status",
+                        healthReportEntry.Value.Status.ToString());
+                    jsonWriter.WriteString("description",
+                        healthReportEntry.Value.Description);
+                    jsonWriter.WriteStartObject("data");
+
+                    foreach (var item in healthReportEntry.Value.Data)
+                    {
+                        jsonWriter.WritePropertyName(item.Key);
+
+                        JsonSerializer.Serialize(jsonWriter, item.Value,
+                            item.Value?.GetType() ?? typeof(object));
+                    }
+
+                    jsonWriter.WriteEndObject();
+                    jsonWriter.WriteEndObject();
+                }
+
+                jsonWriter.WriteEndObject();
+                jsonWriter.WriteEndObject();
+            }
+
+            return context.Response.WriteAsync(
+                Encoding.UTF8.GetString(memoryStream.ToArray()));
+        }
+    }
+}
--- StringCalculatorAPI/Program.cs
+++ StringCalculatorAPI/Program.cs
@@ -1,15 +1,18 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using StringCalculator;
 using StringCalculator.Persistance;
+using StringCalculatorAPI;
 using StringCalculatorAPI.Controllers;
+using System.Runtime.CompilerServices;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-var config = builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+var config = builder.Configuration.SetBasePath(Environment.CurrentDirectory)
     .AddJsonFile("appsettings.json",true,false)
     .AddJsonFile("appsettings.Development.json")
     .Build();
@@ -17,8 +20,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<DatePicker,APIDatePicker>();
 builder.Services.AddScoped<Save,HistoryStorer>(services => new HistoryStorer(config["Path"]));
 builder.Services.AddScoped<HistoryHandler,HistoryHandler>(services => new HistoryHandler(new HistoryStorer(config["Path"]),services.GetRequiredService<DatePicker>()));
+builder.Services.AddHealthChecks().AddCheck("HistoryCheck",new HistoryHealthCheck(config["Path"]));
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,5 +35,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = HistoryHealthCheck.writeJson
+});
 
 app.Run();
